700829fa1a481949e24e3d088203830db9d55e61
cristhian ochoa <cristhho@gmail.com>
2021-07-16 15:31:49 -0500

    remove item from cart

--- src/components/layout/CartButton.js
+++ src/components/layout/CartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import CartIcon from '../cart/CartIcon';
 import CartContext from '../../store/cart-context';
@@ -6,13 +6,28 @@ import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
   const ctx = useContext(CartContext);
+  const [animateButton, setAnimateButton] = useState(false);
 
   const totalCartItems = ctx.items.reduce((total, item) => {
     return total + item.amount;
   }, 0);
 
+  const buttonClasses = `${classes.button} ${animateButton ? classes.bump : ''}`;
+  const items = ctx.items;
+
+  useEffect(() => {
+    if (items.length === 0) return;
+    setAnimateButton(true);
+    const timer = setTimeout(() => {
+      setAnimateButton(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={buttonClasses} onClick={props.onClick}>
       <span className={classes.icon}><CartIcon /></span>
       <span>Your cart</span>
       <span className={classes.badge}>{totalCartItems}</span>
--- src/store/cart-provider.js
+++ src/store/cart-provider.js
@@ -26,6 +26,23 @@ const cartReducer = (state, action) => {
       items: updatedItems,
       totalAmount: updatedAmount
     };
+  } else if (action.type === 'REMOVE') {
+    const existingCartItemIndex = state.items.findIndex((item) => item.id === action.id);
+    const existingCartItem = state.items[existingCartItemIndex];
+    const updatedAmount = state.totalAmount - existingCartItem.price;
+    let updatedItems;
+    if (existingCartItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = {...existingCartItem, amount: existingCartItem.amount - 1};
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedAmount
+    };
   }
   return initialState;
 };
