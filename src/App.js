33f22872f157a6c3e2dac297acb88308574bf70d
cristhian ochoa <cristhho@gmail.com>
2021-07-29 15:53:09 -0500

    send data to api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
@@ -13,21 +14,22 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films');
+      const response = await fetch('https://react-burger-app-bb.firebaseio.com/movies.json');
 
       if (!response.ok) throw new Error(`Code: ${response.status} - Something went wrong!`);
 
       const data = await response.json();
+      const loadedMovies = [];
 
-      const transformedMovies = data.results.map((movie) => {
-        return {
-          id: movie.episode_id,
-          title: movie.title,
-          openingText: movie.opening_crawl,
-          releaseDate: movie.release_date
-        };
-      });
-      setMovies(transformedMovies);
+      for (const movie in data) {
+        loadedMovies.push({
+          id: movie,
+          title: data[movie].title,
+          openingText: data[movie].openingText,
+          releaseDate: data[movie].releaseDate,
+        });
+      }
+      setMovies(loadedMovies);
     } catch (err) {
       setError(err.message);
     }
@@ -38,8 +40,24 @@ function App() {
     fetchMovies();
   }, [fetchMovies]);
 
+  async function addMovieHandler(movie) {
+    const response = await fetch('https://react-burger-app-bb.firebaseio.com/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json();
+    console.log(data);
+    fetchMovies();
+  }
+
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
