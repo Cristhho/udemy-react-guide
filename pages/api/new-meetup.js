85359134737f94736e12d62b14ad163773e58d78
cristhian ochoa <cristhho@gmail.com>
2022-06-08 22:31:45 -0500

    save meet up in mongo db

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,15 +1,16 @@
 import { MongoClient } from 'mongodb';
 
+const connectionURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gq1wj.mongodb.net/meetups?retryWrites=true&w=majority`;
+
 export default async (req, res) => {
   if (req.method === 'POST') {
     const data = req.body;
-    const client = await MongoClient.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gq1wj.mongodb.net/meetups?retryWrites=true&w=majority`);
+    const client = await MongoClient.connect(connectionURL);
     const db = client.db();
 
     const meetupsCollection = db.collection('meetups');
     const result = await meetupsCollection.insertOne(data);
 
-    console.log(result);
     client.close();
 
     res.status(201).json({
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -2,8 +2,17 @@ import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 const NewMeetup = () => {
 
-  const addMeetupHandler = (meetup) => {
-    console.log(meetup)
+  const addMeetupHandler = async (meetup) => {
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(meetup),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+    console.log(data);
   }
   
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />
